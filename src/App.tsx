efbecf94c9514bd06d827f80830f98106c4a7e07
Anuj Singh <kumaranuj1303@gmail.com>
2024-04-13 14:20:48 +0530

    project intialize

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center my-4">
+        Vite + Tailwind + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
