de3a05fb37c0888671f45cdc29a18d9a26cc0f47
Anuj Singh <kumaranuj1303@gmail.com>
2024-04-15 17:23:54 +0530

    update heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-center my-4">
-        Vite + Tailwind + React
+        Vite + Tailwind + React + GitHub Actions
       </h1>
     </>
   );
